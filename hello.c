0f726e4f815b25097e4023a8c96da299c1932ca0
Karunesh Johri <kj@softprayog.in>
2021-07-18 16:00:42 +0530

    Major change

--- hello.c
+++ hello.c
@@ -1,10 +1,29 @@
-#if HAVE_CONFIG_H 
-#include <config.h> 
-#endif 
-#include <stdio.h>
-#include <stdlib.h>
+/*
 
-main ()
+    hello.c: Print "Hello, World!" message
+    Copyright (C) 2021  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    */
+#include <stdio.h>
+
+int main (int argc, char **argv)
 {
-    printf ("Hello, the brave new World!\n");
+    printf ("Good morning!\n");
+    printf ("Hello, World!\n");
+    printf ("All the best!\n");
+    printf ("Good evening!\n");
+    printf ("Good night!\n");
 }
